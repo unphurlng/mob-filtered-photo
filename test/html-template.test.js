7cdf5eb130ba8d0f597704c2a3f868c26d341313
heather <unphurlng@gmail.com>
2019-02-26 14:30:11 -0800

    dynamically displaying URL and title TDD green

--- test/html-template.test.js
+++ test/html-template.test.js
@@ -2,21 +2,23 @@ const test = QUnit.test;
 
 QUnit.module('URL and title:');
 
-function createTemplate() {
+const image = {
+    title: 'UniWhal',
+    url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
+};
+
+function createTemplate(image) {
     return `
         <li>
-            <p>UniWhal</p>
-            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+            <p>${image.title}</p>
+            <img src="${image.url}">
         </li>
         `;
 }
 
 test('dynamically displays images and their keys', assert => {
     //arrange
-    const images = {
-        title: 'UniWhal',
-        url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
-    };
+
     const expected = `
         <li>
             <p>UniWhal</p>
@@ -25,7 +27,7 @@ test('dynamically displays images and their keys', assert => {
         `;
 
     //act
-    const result = createTemplate();
+    const result = createTemplate(image);
 
     //assert
     assert.equal(result, expected);
