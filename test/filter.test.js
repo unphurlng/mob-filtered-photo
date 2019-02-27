0438155a1ad01cab2a80dc1e59e6d4a9c40a27cf
heather <unphurlng@gmail.com>
2019-02-26 16:35:17 -0800

    horns test passing using the function

--- test/filter.test.js
+++ test/filter.test.js
@@ -8,23 +8,36 @@ const images = [
     { title: 'image3', keyword: 'green', horns: 2 },
 ];
 
-const filtered = { keyword: 'blue' };
 
 function filterImages(images, filtered) {
     return images.filter((image) => {
-        const hasKeyword = image.keyword === filtered.keyword;
-        return hasKeyword;  
+        const hasKeyword = !filtered.keyword || image.keyword === filtered.keyword;
+        
+        const hasHorns = !filtered.horns || image.horns >= filtered.horns;
+        return hasKeyword && hasHorns;  
     });
 }
 
 test('get the right array using keyword', assert => {
-    //arrange
     const expected = [
         { title: 'image1', keyword: 'blue', horns: 1 },
         { title: 'image2', keyword: 'blue', horns: 2 },
     ];
-    //act
+
+    const filtered = { keyword: 'blue' };
     const result = filterImages(images, filtered);
-    //assert
+
+    assert.deepEqual(result, expected);
+});
+
+test('get horns property', assert => {
+    const expected = [
+        { title: 'image2', keyword: 'blue', horns: 2 },
+        { title: 'image3', keyword: 'green', horns: 2 }
+    ];
+
+    const filtered = { keyword: '', horns: 2 };
+    const result = filterImages(images, filtered);
+
     assert.deepEqual(result, expected);
 });
