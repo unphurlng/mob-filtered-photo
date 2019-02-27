3bc96fd927c3c2b04baecd1b0e78908c243715c5
heather <unphurlng@gmail.com>
2019-02-26 16:06:25 -0800

    Pass first test on filter

--- test/filter.test.js
+++ test/filter.test.js
@@ -6,8 +6,16 @@ const images = [
     { title: 'image1', keyword: 'blue', horns: 1 },
     { title: 'image2', keyword: 'blue', horns: 2 },
     { title: 'image3', keyword: 'green', horns: 2 },
-
 ];
+
+const filtered = { keyword: 'blue' };
+
+function filterImages(images, filtered) {
+    return images.filter((image) => {
+        const hasKeyword = image.keyword === filtered.keyword;
+        return hasKeyword;  
+    });
+}
 
 test('get the right array using keyword', assert => {
     //arrange
@@ -16,7 +24,7 @@ test('get the right array using keyword', assert => {
         { title: 'image2', keyword: 'blue', horns: 2 },
     ];
     //act
-    const result = filter(images);
+    const result = filterImages(images, filtered);
     //assert
     assert.deepEqual(result, expected);
 });
